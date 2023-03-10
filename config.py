489f2d6ca74f8df2f211b98ca7cfa8b2a49e48eb
ediazgarcia <eddymanueldiazgarcia@gmail.com>
2023-03-10 01:18:47 -0400

    configuracion de proyecto flask

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+user = os.environ['MYSQL_USER']
+password = os.environ['MYSQL_PASSWORD']
+host = os.environ['MYSQL_HOST']
+database = os.environ['MYSQL_DATABASE']
+# port = os.environ['MYSQL_PORT']
+
+DATABASE_CONNECTION_URI = f'mysql://{user}:{password}@{host}/{database}'
+
+print(DATABASE_CONNECTION_URI)
