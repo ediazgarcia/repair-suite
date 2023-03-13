54afb307109ce2c52b7563d5c5eccc6ce2513b8d
ediazgarcia <eddymanueldiazgarcia@gmail.com>
2023-03-13 16:04:51 -0400

    Primera part. cruds mant. clientes

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from routes.clients import clients
-from flask_sqlalchemy import SQLAlchemy
 from config import DATABASE_CONNECTION_URI
 
 app = Flask(__name__)
 
-app.secret_key = "secret key"
+# settings
+app.config['SECRET_KEY'] = 'dev'
+print(DATABASE_CONNECTION_URI)
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_CONNECTION_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-SQLAlchemy(app)
+
+# no cache
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+# SQLAlchemy(app)
 
 app.register_blueprint(clients)
--- index.py
+++ index.py
@@ -1,8 +1,9 @@
 from app import app
 from utils.db import db
-import config
 
-# with app.app_context(): db.create_all()
+with app.app_context():
+    db.create_all()
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
