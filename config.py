0d0cc68a9894a22813b0e6701770da3b1d5d7647
ediazgarcia <eddymanueldiazgarcia@gmail.com>
2023-03-13 23:38:41 -0400

    Restructuracion del templates

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ load_dotenv()
 user = os.environ['MYSQL_USER']
 password = os.environ['MYSQL_PASSWORD']
 host = os.environ['MYSQL_HOST']
+port = os.environ['MYSQL_PORT']
 database = os.environ['MYSQL_DATABASE']
-# port = os.environ['MYSQL_PORT']
 
-DATABASE_CONNECTION_URI = f'mysql://{user}:{password}@{host}/{database}'
+DATABASE_CONNECTION_URI = f'mysql://{user}:{password}@{host}:{port}/{database}'
 
 print(DATABASE_CONNECTION_URI)
